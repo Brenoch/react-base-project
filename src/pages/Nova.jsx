4d9fb9a5839ca512a7f970b4715e3d9b95d87719
BRENO CHAVES DA CUNHA <202402798502@alunos.ibmec.edu.br>
2024-10-03 17:03:49 -0300

    nova

---
+++ src/pages/Nova.jsx
@@ -0,0 +1,12 @@
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+
+const Nova = () => {
+    return (<>
+    <h1>Titulo da pagina</h1>
+     <p>Ola Mundo</p>
+     <NovoComponente />
+     </>);
+}
+
+export default Nova;
+
